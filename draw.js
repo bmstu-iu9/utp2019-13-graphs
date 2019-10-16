006cd592af6cc6c0df4f846840182eceb546de75
Marina-Glush <morpha_gl@mail.ru>
2019-10-16 10:25:55 +0300

    Исправлена ошибка с зависанием на функции Math.sin(1/x)

--- draw.js
+++ draw.js
@@ -140,36 +140,51 @@ const drawGraph = () =>{
 			let realPrevX = minX;
 			let realCurX = realPrevX;
 			while( realCurX <= maxX) {
-				drawPoint(realPrevX, realCurX);
+				drawPointLimit(realPrevX, realCurX, 2*clientHeight);
 				realPrevX = realCurX;
 				realCurX = realPrevX+deltaX;
 			}
 			ctx.stroke();
 };
 
-const drawPoint = (realPrevX, realCurX) => {
-	const realPrevY = expr(realPrevX);
-	const realCurY = expr(realCurX);
-    if ( checkRealY(realPrevY) || checkRealY(realCurY)) {
-		const scrPrevY = yChangeToScreen(realPrevY);
-		const scrCurY = yChangeToScreen(realCurY);
-		const scrMaxDeltaY = 1; // чтобы линия получилась сплошная
-		if ( Math.abs(scrCurY - scrPrevY) <= scrMaxDeltaY) {
-			// Чертим
-			const scrPrevX = xChangeToScreen(realPrevX);
-			const scrCurX = xChangeToScreen(realCurX);
-			
-			// Выводит точки, если scrMaxDeltaY == 1, то получится сплошная линия
-			ctx.fillRect(scrCurX, scrCurY, 1, 1);
-		} else {
-			// Делим на 2 отрезка
-			const realMidX = (realPrevX + realCurX)/2;
-			drawPoint(realPrevX, realMidX);
-			drawPoint(realMidX, realCurX);
-		}		
-		// else Оба Y выходят за диаппазон видимости чертить ничего не надо
+const epsEqu = (x, y) => {
+    return Math.abs(x - y) < Number.EPSILON * Math.max(Math.abs(x), Math.abs(y));
+}
 
+const drawPointLimit = (realPrevX, realCurX, maxDrawPoint) => {
+	let countDrawPoiunt = 0;
+
+	const drawPoint = (realPrevX, realCurX) => {
+		if (countDrawPoiunt >= maxDrawPoint ) {
+			return; // Если у нас колебaтельные движения, то надо ограничить количество рассчитываемых точек
+		}
+		const realPrevY = expr(realPrevX);
+		const realCurY = expr(realCurX);
+		if ((checkRealY(realPrevY) || checkRealY(realCurY))) {
+			const scrPrevY = yChangeToScreen(realPrevY);
+			const scrCurY = yChangeToScreen(realCurY);
+			const scrMaxDeltaY = 1; // чтобы линия получилась сплошная
+			if ( Math.abs(scrCurY - scrPrevY) <= scrMaxDeltaY) {
+				const scrPrevX = xChangeToScreen(realPrevX);
+				const scrCurX = xChangeToScreen(realCurX);
+
+				// Выводит точки, если scrMaxDeltaY == 1, то получится сплошная линия
+				ctx.fillRect(scrCurX, scrCurY, 1, 1);
+				++countDrawPoiunt;
+			} else {
+				if (epsEqu(realCurX,realPrevX)) {
+					// Мы достигли предела точности
+					drawPoint(realCurX, realCurX);
+				} else {			
+					// Делим на 2 отрезка
+					const realMidX = (realPrevX + realCurX)/2;
+					drawPoint(realPrevX, realMidX);
+					drawPoint(realMidX, realCurX);
+				}
+			}		
+		}
 	}
+	drawPoint(realPrevX, realCurX);
 }
 
 const checkRealY = (realY) => {
@@ -184,12 +199,12 @@ const checkRealY = (realY) => {
 
 //переводит координату х функции в координату х экрана
 const xChangeToScreen = (x) =>{
-	return (x-minX)*scaleX + indent;
+	return Math.round((x-minX)*scaleX + indent);
 }
 
 //переводит координату y функции в координату y экрана
 const yChangeToScreen = (y) =>{
-	return (maxY - y)*scaleY + indent;
+	return Math.round((maxY - y)*scaleY + indent);
 }
 
 //проверка галочки auto
